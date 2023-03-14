5283ba4b9ea48704c0c8aeaa2267323b471e3fcb
MarceloMogli <moglimarcelo@gmail.com>
2023-03-14 03:19:17 +0000

    up to rentals start

--- src/components/LessonsCard/LessonsCard.jsx
+++ src/components/LessonsCard/LessonsCard.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import { urlFor } from '../../client';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../utils/motion';
 import './LessonsCard.scss'
 
 const LessonsCard = (lessonData) => {
     console.log(lessonData);
-    const { details, name, numlessons, price, imgUrl } = lessonData?.lessonData
+    const { details, name, numlessons, price, imgUrl, groupSize } = lessonData?.lessonData
     return (
-        <div className="lesson__card">
+        <motion.div 
+            variants={fadeIn('right', 'easeInOut', 0.5, 0.75)}
+        className="lesson__card">
             <div className="lesson-card__image">
                 <img src={urlFor(imgUrl)} alt={name} />
             </div>
             <div className='lesson__card-body'>
                 <h3 className="lesson__card-title">{name}</h3>
+                <p className="lesson__card-text"><span className='lesson__span-text'>Num of Lessons:</span> {numlessons}</p>
                 <p className="lesson__card-text">{details}</p>
-                <p className="lesson__card-text">Num of Lessons: {numlessons}</p>
-                <p className="lesson__card-text">Price: {price}€</p>
+                <p className="lesson__card-text"><span className='lesson__span-text'>Group Size:</span> {groupSize}</p>
+                <p className="lesson__card-text"><span className='lesson__span-text'>Price:</span> {price}</p>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/containers/Rentals/Rentals.jsx
+++ src/containers/Rentals/Rentals.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { staggerContainer, fadeIn } from '../../utils/motion'
+import { TypingText } from '../../components'
+import './Rentals.scss'
 
 const Rentals = () => {
   return (
-    <div>Rentals</div>
+    <section id='Rentals'>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="rentals__container"
+      >
+        <TypingText title="Rentals" />
+        <motion.div
+          className='rentals__subtitle-container'
+          variants={fadeIn('up', 'tween', 0.2, 1)}
+        >
+          <p className='rentals__subtitle'>
+            Our rental equipment is carefully maintained and regularly
+            updated to ensure that you have the best experience possible.
+            We offer a variety of surfboard sizes and styles to fit your skill
+            level and preferred surfing conditions. In addition to surfboards, we
+            also offer wetsuits, leashes, and other surf accessories.
+          </p>
+        </motion.div>
+      </motion.div>
+    </section>
   )
 }
 

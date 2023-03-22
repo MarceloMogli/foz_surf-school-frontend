ee246c4a28df34df5dea7034808a3cdc84b4402d
MarceloMogli <moglimarcelo@gmail.com>
2023-03-22 18:59:18 +0000

    s

--- src/components/LessonsCard/LessonsCard.jsx
+++ src/components/LessonsCard/LessonsCard.jsx
@@ -13,7 +13,7 @@ const LessonsCard = (lessonData) => {
             </div>
             <div className='lesson__card-body'>
                 <h3 className="lesson__card-title">{name}</h3>
-                <p className="lesson__card-text"><span className='lesson__span-text'>Num of Lessons:</span> {numlessons}</p>
+                <p className="lesson__card-text"><span className='lesson__span-text'>Surf Sessions:</span> {numlessons}</p>
                 <p className="lesson__card-text">{details}</p>
                 <p className="lesson__card-text"><span className='lesson__span-text'>Group Size:</span> {groupSize}</p>
                 <p className="lesson__card-text"><span className='lesson__span-text'>Price:</span> {price}</p>
